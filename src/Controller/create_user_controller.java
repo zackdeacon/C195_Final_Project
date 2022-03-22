da56ffbefd757c9256c3fd2110e2b19206555990
zackdeacon <zackdeacon347@gmail.com>
2022-03-21 23:07:54 -0400

    finished create user functionality

--- src/Controller/create_user_controller.java
+++ src/Controller/create_user_controller.java
@@ -1,9 +1,15 @@
 package Controller;
 
 import Database.UserDao;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,9 +29,14 @@ public class create_user_controller implements Initializable {
         System.out.println("Initialized");
     }
 
-    public void makeUser() throws Exception {
+    public void makeUser(ActionEvent actionEvent) throws Exception {
         UserDao.createUser(userNameText.getText(), passwordText.getText());
-
+        Parent root = FXMLLoader.load(getClass().getResource("/view/log-in_form.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 600, 400);
+        stage.setTitle("Log-in Page");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void exitProgram() {
--- src/Database/UserDao.java
+++ src/Database/UserDao.java
@@ -9,26 +9,35 @@ public class UserDao {
 
     public static user getUser(String userName) throws SQLException, Exception {
         JDBC.getConnection();
-            String sqlStmt = "SELECT * FROM users WHERE User_Name = '" + userName + "'";
-            Query.makeQuery(sqlStmt);
-            user userResult;
-            ResultSet result = Query.getResult();
-            while(result.next()) {
-                int userID = result.getInt("User_ID");
-                String userNAME = result.getString("User_Name");
-                String password = result.getString("Password");
-                userResult = new user(userID, userNAME, password);
-                return userResult;
-            }
+        String sqlStmt = "SELECT * FROM users WHERE User_Name = '" + userName + "'";
+        Query.makeQuery(sqlStmt);
+        user userResult;
+        ResultSet result = Query.getResult();
+        while (result.next()) {
+            int userID = result.getInt("User_ID");
+            String userNAME = result.getString("User_Name");
+            String password = result.getString("Password");
+            userResult = new user(userID, userNAME, password);
+            return userResult;
+        }
 
-            return null;
+        return null;
 
     }
 
-    public static void createUser(String userName, String password) throws SQLException, Exception {
-        //JDBC.getConnection();
-        String sqlStmt = "INSERT INTO users (User_Name, Password, Create_Date, Created_By, Last_Update, Last_Updated_By) VALUES('" + userName + "','" + password + "')";
-        System.out.println(sqlStmt);
+    public static boolean createUser(String userName, String password) throws SQLException, Exception {
+        try {
+            JDBC.getConnection();
+            String sqlStmt =
+                    "INSERT INTO users (User_Name, Password, Create_Date, Created_By, Last_Update, Last_Updated_By) VALUES('"
+                            + userName + "','" + password + "', sysdate(), '" + userName + "', sysdate(), '" + userName + "')";
+            //System.out.println(sqlStmt);
+            Query.makeQuery(sqlStmt);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+
     }
 
 }
